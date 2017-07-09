a2aaa6d7edc5c36a02ef5bb62c0ceac45e62bc47
nvagus <togemig@hotmail.com>
2017-07-08 22:11:53 -0700

    rm toc

--- AllCodes/lab1_program09_lcd/lab1_program09_lcd.ino
+++ AllCodes/lab1_program09_lcd/lab1_program09_lcd.ino
@@ -14,8 +14,24 @@
 
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7, 8); // port 1
-int buttonLeft = 10;
-int buttonRight = 9;
-int col = 0;
-int row = 0;
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7, 8);     // Bus 1 of the Seeduino sensor Chasis
+#define BUTTON_LEFT 11
+#define BUTTON_RIGHT 12
+int col = 0;                                // column of where the cursor should be
+int row = 0;                                // row of where the cursor should be
+
+void moveLeft() {                           // move the cursor left
+    lcd.clear();                            // clear the output 'X' on lcd
+    if (col > 1)                            // if 
+        col -= 1;                           // let the column of the cursor down by 1
+    else if (row > 0) {
+        row -= 1;
+        col = 16;
+    }
+    lcd.setCursor(col, row);
+    lcd.print("X");
+}
+
+
+
+
