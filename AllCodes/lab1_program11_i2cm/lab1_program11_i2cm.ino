110da7dde607fb10d8c135793d11e22a4d77c6e2
nvagus <togemig@hotmail.com>
2017-07-09 00:15:04 -0700

    modified abstract

---
+++ AllCodes/lab1_program11_i2cm/lab1_program11_i2cm.ino
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------
+//  Module name:
+//      lab1_program11_i2cm.ino
+//  Languange:
+//      Wiring/Arduino
+//  Description:
+//      The program is deployed on the master of the I2C wire. The
+//      master device send signals of its button to the slave device,
+//      controlling it to switch on or off its LED.
+//  Author:
+//      Mingxiao An, Man Sun, Muhan Li
+//  Rev.0 28 June 2017
+//  Rev.1 8 July 2017
+//----------------------------------------------------------------
+
+#include <Wire.h>
+
+#define I2C 4                   // using analog port 4 as i2c bus, port 5 should also be connected
+#define BUTTON 12               // pin 12 is the pin attaching the button
+
+// the setup routine runs once when you press reset:
+void setup() {
+    pinMode(BUTTON, INPUT);     // initialize the digital pin as an input
+    Wire.begin();               // join i2c bus (address optional for master)
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    Wire.beginTransmission(I2C);                // transmit to device #4
+    Wire.write(digitalRead(BUTTON) != 0);       // send the signal of whether the LED is on
+    Wire.endTransmission();                     // stop transmitting
+}
+
---
+++ AllCodes/lab1_program11_i2cs/lab1_program11_i2cs.ino
@@ -0,0 +1,42 @@
+//----------------------------------------------------------------
+//  Module name:
+//      lab1_program11_i2cs.ino
+//  Languange:
+//      Wiring/Arduino
+//  Description:
+//      The program is deployed on the slave of the I2C wire. The
+//      device receive signals from I2C wire, and switch on or off 
+//      its LED by the result of the signals. 
+//  Author:
+//      Man Sun, Mingxiao An, Muhan Li
+//  Rev.0 28 June 2017
+//  Rev.1 8 July 2017
+//----------------------------------------------------------------
+
+#include <Wire.h>
+
+#define I2C 4                   // using analog port 4 as i2c bus, port 5 should also be connected
+#define LED 10                  // pin 10 is the pin attaching the LED
+
+// the setup routine runs once when you press reset:
+void setup() {
+    pinMode(LED, OUTPUT);           // initialize the digital pin as an output
+    Wire.begin(I2C);                  // join i2c bus with address #4
+    Wire.onReceive(receiveEvent);   // register event
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    delay(100);
+}
+
+// this function is registered as an event:
+void receiveEvent(int howMany) {
+    byte on = Wire.read();              // read the input as byte
+    if (on) {
+        digitalWrite(LED, HIGH);        // turn the LED on
+    } else {
+        digitalWrite(LED, LOW);         // turn the LED off
+    }
+}
+
